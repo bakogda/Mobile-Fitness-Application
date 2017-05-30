068e44cc86e6bdba3386cacef4cafecc5e618011
Unknown <bakogda@gmail.com>
2017-05-30 22:44:15 +0100

    re #3 Changes to the Database

--- app/src/main/java/bako/fitness/AppConfig.java
+++ app/src/main/java/bako/fitness/AppConfig.java
@@ -5,4 +5,9 @@ package bako.fitness;
  */
 
 public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://10.0.2.2:80/fitness/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://10.0.2.2:80/fitness/register.php";
 }
